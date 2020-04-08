da61267222dd721298724a78a4b63d70c1706c2d
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-07 22:17:27 -0400

    build boilerplate ToDoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,3 +1,11 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
 
+export default function ToDoList() {
+  return (
+    <div>
+      <p>to do list</p>
+    </div>
+  )
+}
