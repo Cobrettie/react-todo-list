fcf7798f3bf1e1bb39582ce4954603b5c881588b
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-07 22:01:49 -0400

    add array to state, add object to that array for testing purposes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   constructor() {
     super();
-    this.state = {}
+    this.state = [
+      {
+        task: 'test todo list',
+        id: 123456,
+        completed: false;
+      }
+    ]
   }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
