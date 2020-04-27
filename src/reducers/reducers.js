19d76ce697d08efb251c6d672144644f921d63b1
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-27 13:30:07 -0400

    remove unneeded code from reducers.js

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,21 +1,6 @@
 import { combineReducers } from 'redux';
 import todoItemsArray from './todoItemsArray';
 
-export const initialState = {
-  todoItems: [
-    {
-      id: 0,
-      task: 'test item 1 ',
-      completed: false
-    },
-    {
-      id: 1,
-      task: 'testy item 2',
-      completed: false
-    }
-  ]
-}
-
 export const reducer = combineReducers({
   todoItemsArray
 })
