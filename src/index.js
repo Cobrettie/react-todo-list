ac2b3899ec86b0f6f30321513457708e43aebff3
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-27 21:25:47 -0400

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,10 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer } from './reducers/reducers';
-import { addTodo } from './actions/actions';
 
 import App from './App';
 
 const store = createStore(reducer)
-
-
-
-
-
-console.log('getting initialState', store.getState())
-
-const unsubscribe = store.subscribe(() => console.log('unsibscribe', store.getState))
-
-store.dispatch(addTodo('using store.dispatch() in index.js to add this item'))
-
-unsubscribe()
-
-
-
-
 
 ReactDOM.render(
   <Provider store={store}>
