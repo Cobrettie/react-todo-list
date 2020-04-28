267a3d1a2ab8c413a1227c2606571500d6c5df18
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-27 21:33:02 -0400

    style Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,22 @@
 import React from 'react';
+
 import FilterLink from '../ContainerComponents/FilterLink';
 import { VisibilityFilters } from '../actions/actions';
 
+import styled from 'styled-components';
+
+const FooterContainerDiv = styled.div`
+  margin: 20px auto 0;
+`;
+
 function Footer() {
   return (
-    <div>
+    <FooterContainerDiv>
       <span>Show: </span>
       <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
       <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
       <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
-    </div>
+    </FooterContainerDiv>
   )
 }
 
